4f9756a33089613808df1742ecd5c738e761ea59
andresraul <andresraulvaras@yahoo.com>
2022-09-26 21:48:23 -0300

    Proyecto nombre página Semitas Caseras

---
+++ script-semitas.js
@@ -0,0 +1,21 @@
+
+
+var numero = 1;
+
+function adelante() {
+    numero++;
+    if(numero > 7) 
+        numero = 1;
+        var foto = document.getElementById("foto");
+        foto.src = "semitas/semita-" + numero + ".jpg";
+    
+}
+
+function atras() {
+    numero--;
+    if(numero < 1) 
+        numero = 7;
+        var foto = document.getElementById("foto");
+        foto.src = "semitas/semita-" + numero + ".jpg";
+    
+}
